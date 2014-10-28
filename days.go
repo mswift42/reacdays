f56bcd3a415edc8e434df2b55489cb7b316f9876
martin.haesler <martin.haesler@gmail.com>
2014-10-28 09:56:19 +0000

    Add tasks handler

--- days.go
+++ days.go
@@ -4,7 +4,9 @@ import (
 	"appengine"
 	"appengine/datastore"
 	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
 	"time"
 )
 
@@ -38,6 +40,10 @@ func (t *Task) save(c appengine.Context) (*Task, error) {
 	return t, nil
 }
 
+func (t *Task) delete(c appengine.Context) error {
+	return datastore.Delete(c, t.key(c))
+}
+
 func decodeTask(r io.ReadCloser) (*Task, error) {
 	defer r.Close()
 	var task Task
@@ -55,4 +61,41 @@ func listTasks(c appengine.Context) ([]Task, error) {
 		tasks[i].Id = keys[i].IntID()
 	}
 	return tasks, nil
+}
+
+func init() {
+	http.HandleFunc("/tasks", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	val, err := handleTasks(c, r)
+	if err == nil {
+		json.NewEncoder(w).Encode(val)
+	}
+	if err != nil {
+		c.Errorf("Task error : %#v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func handleTasks(c appengine.Context, r *http.Request) (interface{}, error) {
+	switch r.Method {
+	case "POST":
+		task, err := decodeTask(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return task.save(c)
+	case "GET":
+		return listTasks(c)
+	case "DELETE":
+		task, err := decodeTask(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		return nil, task.delete(c)
+	}
+	return nil, fmt.Errorf("method not implemented")
 }
