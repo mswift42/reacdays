6c997ef233bef8594825e584d297f7f843e03452
martin.haesler <martin.haesler@gmail.com>
2014-10-30 17:20:10 +0000

    add /jsx folder

---
+++ app/jsx/app.jsx
@@ -0,0 +1,13 @@
+/** @jsx React.DOM */
+'use strict';
+define([], function () {
+        return React.createClass({
+            render: function() {
+                return (
+                    <div className="taskoverview">
+                    <p>I am a placeholder</p>
+                    </div>
+                    );
+                }
+        });
+});
---
+++ app/jsx/main1.jsx
@@ -0,0 +1,5 @@
+/** @jsx React.DOM */
+React.renderComponent(
+    <h1>Days</h1>,
+    document.getElementById('title')
+);
