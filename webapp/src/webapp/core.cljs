62be3ffd9124f997a01702dea78d5209883a7e8a
martin.haesler <martin.haesler@gmail.com>
2014-11-04 18:34:50 +0000

    Use reagent-seed template

--- webapp/src/webapp/core.cljs
+++ webapp/src/webapp/core.cljs
@@ -1,5 +1,18 @@
-(ns webapp.core)
+(ns webapp.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [webapp.session :as session :refer [get-state]]
+            [webapp.routes :as routes]
+            [webapp.views.common :as common]))
 
-(enable-console-print!)
+(defn page-render []
+  [:div
+   [common/header]
+   [(get-state :current-page)]])
 
-(println "Hello world!")
+(defn page-component [] 
+  (reagent/create-class {:component-will-mount routes/app-routes
+                         :render page-render}))
+
+;; initialize app
+(reagent/render-component [page-component]
+                          (.getElementById js/document "app"))
