38ae6ec7355e8b2032ac6b26c8683f93d5f90f3c
martin.haesler <martin.haesler@gmail.com>
2014-11-10 17:40:13 +0000

    move newtask form to newtask_page

--- webapp/src/webapp/views/home_page.cljs
+++ webapp/src/webapp/views/home_page.cljs
@@ -1,7 +1,26 @@
-(ns webapp.views.home-page)
+(ns webapp.views.home-page
+  (:require [ajax.core :refer [GET POST]]
+            [reagent.core :as reagent]))
+
+(defn handler [response]
+  (.log js/console (str response)))
+
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "error happended: " status " " status-text)))
+
+
+(defn list-tasks
+  []
+  (GET "/tasks" {:handler handler
+                 :error-handler error-handler}))
+
+(defn post-task
+  [task]
+  (POST "/tasks" {:handler handler
+                  :error-handler error-handler}))
 
 (defn home-page []
   [:div
-   [:h2 "Home Page"]
-   [:div "Woot! You are starting a reagent application."]
-   ])
+   [:h2.header "Tasks"]
+   [:div.container
+    [:div#taskoverview {:class-name "taskoverview"}]]])
--- webapp/src/webapp/views/newtask_page.cljs
+++ webapp/src/webapp/views/newtask_page.cljs
@@ -1,7 +1,26 @@
 (ns webapp.views.newtask-page)
 
+
+(defn row [label input]
+  [:div.row
+   [:div.col-md-2 [:label label]]
+   [:div.col-md-5 input]])
+
+(def form-template
+  [:div
+   (row "Summary"
+        [:input.form-control {:field :text :id :summary}])
+   (row "Content"
+        [:textarea.form-control {:field :textarea :id :content}])
+   (row "Scheduled"
+        [:input.form-control {:field :text :id :scheduled}])
+   (row ""
+        [:button "Save"])])
+
+
 (defn newtask-page []
   [:div.container
    [:h2.header "New Task"]
    [:div.newtaskform
-    [:p "hier kommen tasks hin"]]])
+    form-template
+    ]])
