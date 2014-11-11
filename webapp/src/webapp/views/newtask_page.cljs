2be468775ac610b0f36120599944a26361698c65
martin.haesler <martin.haesler@gmail.com>
2014-11-11 18:10:03 +0000

    add on-submit fn

--- webapp/src/webapp/views/newtask_page.cljs
+++ webapp/src/webapp/views/newtask_page.cljs
@@ -1,10 +1,12 @@
-(ns webapp.views.newtask-page)
+(ns webapp.views.newtask-page
+  (:require [ajax.core :refer [POST]]))
 
 
 (defn row [label input]
   [:div.row
    [:div.col-md-2 [:label label]]
    [:div.col-md-5 input]])
+
 
 (def form-template
   [:div
@@ -15,12 +17,11 @@
    (row "Scheduled"
         [:input.form-control {:field :text :id :scheduled}])
    (row ""
-        [:button "Save"])])
+        [:button "Save" {:on-submit (POST "/tasks" {:params (js/FormData. form-element)}) }])])
 
 
 (defn newtask-page []
   [:div.container
    [:h2.header "New Task"]
    [:div.newtaskform
-    form-template
-    ]])
+    form-template]])
