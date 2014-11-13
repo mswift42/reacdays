0532ac6769885ea40aa4884e32504d6b930c3a52
martin.haesler <martin.haesler@gmail.com>
2014-11-13 15:55:12 +0000

    use ajax-request fn from cljs-ajax

--- webapp/src/webapp/views/newtask_page.cljs
+++ webapp/src/webapp/views/newtask_page.cljs
@@ -1,5 +1,6 @@
 (ns webapp.views.newtask-page
-  (:require [ajax.core :refer [POST]]))
+  (:require [ajax.core :refer [ajax-request json-request-format
+                               json-response-format]]))
 
 
 (defn row [label input]
@@ -7,17 +8,36 @@
    [:div.col-md-2 [:label label]]
    [:div.col-md-5 input]])
 
+(defn handler [response]
+  (.log js/console (str response)))
+
+(defn error-handler [{:keys [status status-text]}]
+  (.log js/console (str "error happended: " status " " status-text)))
+
+
+(defn post-task
+  []
+  (ajax-request
+   {:uri  "/tasks"
+    :method :post
+    :params {:summary "summary" :content "content" :scheduled "scheduled"}
+    :format (json-request-format)
+    :response-format (json-response-format {:keywords? true})
+    :handler handler
+    :error-handler error-handler}))
 
 (def form-template
   [:div
-   (row "Summary"
-        [:input.form-control {:field :text :id :summary}])
-   (row "Content"
-        [:textarea.form-control {:field :textarea :id :content}])
-   (row "Scheduled"
-        [:input.form-control {:field :text :id :scheduled}])
-   (row ""
-        [:button "Save" {:on-submit (POST "/tasks" {:params (js/FormData. form-element)}) }])])
+   [:form {:class-Name "form-element"}
+    (row "Summary"
+         [:input.form-control {:field :text :id :summary :name "summary"}])
+    (row "Content"
+         [:textarea.form-control {:field :textarea :id :content :name "content"}])
+    (row "Scheduled"
+         [:input.form-control {:field :text :id :scheduled :name "scheduled"}])
+    (row ""
+         [:input.form-control {:type "button" :value "Save" :on-click (fn []
+                                                                        (post-task))}])]])
 
 
 (defn newtask-page []
